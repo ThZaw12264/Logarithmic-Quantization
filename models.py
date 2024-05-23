47854a724677613495e9a19bc8aacfaed2947d88
Michael Wang <michaelwang9000@gmail.com>
2024-05-22 17:05:06 -0700

    added code for downloading Fashion mnist and training garment classifier

--- models.py
+++ models.py
@@ -1,85 +1,163 @@
-import numpy as np
-import pandas as pd
 
-import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
-from keras.utils import np_utils
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import OneHotEncoder
-import matplotlib.pyplot as plt
-import imageio
-
-from fastai import *
-from fastai.vision import *
-from fastai.vision.all import *
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch
+import torchvision
+import torchvision.transforms as transforms
+# PyTorch TensorBoard support
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
 
 
-df = pd.read_csv('../input/clothing-single-channel/fashion-mnist_train.csv')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-df_x = df.loc[:,'pixel0':'pixel783']
-df_y = df.loc[:,'label']
+# PyTorch models inherit from torch.nn.Module
+class GarmentClassifier(nn.Module):
+    def __init__(self):
+        super(GarmentClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
-np_x = np.array(df_x)
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
 
-X_train = np.array(np_x).reshape(-1,28, 28)
-y_train = np.array(df_y)
 
-print(X_train.shape)
-print(y_train.shape)
+model = GarmentClassifier()
 
-X_train = np.stack((X_train,)*3, axis=-1)
-print(X_train.shape)
 
-def save_imgs(path:Path, data, labels):
-    for label in np.unique(labels):
-        (path/str(label)).mkdir(parents=True,exist_ok=True)
-    for i in range(len(data)):
-        if(len(labels)!=0):
-            imageio.imsave( str( path/str(labels[i])/(str(i)+'.jpg') ), data[i])
-        else:
-            imageio.imsave( str( path/(str(i)+'.jpg') ), data[i])
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))])
 
-save_imgs(Path('/data/digits'),X_train,y_train)
+# Create datasets for training & validation, download if necessary
+training_set = torchvision.datasets.FashionMNIST('./data', train=True, transform=transform, download=True)
+#uncomment this line to make training data set smaller
+# training_set = torch.utils.data.Subset(training_set, range(5000))
+validation_set = torchvision.datasets.FashionMNIST('./data', train=False, transform=transform, download=True)
 
-from keras.applications.resnet50 import preprocess_input
-import tensorflow as tf
+# Create data loaders for our datasets; shuffle for training, not for validation
+training_loader = torch.utils.data.DataLoader(training_set, batch_size=4, shuffle=True)
+validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=4, shuffle=False)
 
-print('total classes :', len(os.listdir('/data/digits')))
-print('Images with label 1: ', len(os.listdir('/data/digits/1')))
+# Class labels
+classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+        'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
 
-print('Image names with label 1')
-print(os.listdir('/data/digits/1')[:10])
+# Report split sizes
+print('Training set has {} instances'.format(len(training_set)))
+print('Validation set has {} instances'.format(len(validation_set)))
 
-train_datagen=tf.keras.preprocessing.image.ImageDataGenerator(preprocessing_function=preprocess_input, validation_split=0.15)
-train_generator = train_datagen.flow_from_directory('/data/digits', class_mode='categorical', subset='training')
-valid_generator = train_datagen.flow_from_directory('/data/digits', class_mode='categorical', subset='validation')
+# print(training_set[0][0].shape)
 
-x, y = train_generator[0]
-print(x.shape)
-print(y.shape)
+loss_fn = torch.nn.CrossEntropyLoss()
 
-resnet_weights_path = '../input/keras-pretrained-models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
+# Optimizers specified in the torch.optim package
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-from tensorflow.keras.applications.resnet50 import ResNet50
-model = Sequential()
 
-model.add(ResNet50(include_top = False, pooling = 'avg', weights = 'imagenet'))
+loss_fn = torch.nn.CrossEntropyLoss()
 
-# 2nd layer as Dense for 2-class classification, i.e., dog or cat using SoftMax activation
-model.add(Dense(10, activation = 'softmax'))
+# Optimizers specified in the torch.optim package
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-# Say not to train first layer (ResNet) model as it is already trained
-model.layers[0].trainable = False
+def train_one_epoch(epoch_index, tb_writer):
+    running_loss = 0.
+    last_loss = 0.
 
-model.summary()
+    # Here, we use enumerate(training_loader) instead of
+    # iter(training_loader) so that we can track the batch
+    # index and do some intra-epoch reporting
+    for i, data in enumerate(training_loader):
+        # Every data instance is an input + label pair
+        inputs, labels = data
 
-model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
 
-model.fit_generator(generator=train_generator,epochs=10,validation_data=valid_generator)
+        # Make predictions for this batch
+        outputs = model(inputs)
+
+        # Compute the loss and its gradients
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+
+        # Adjust learning weights
+        optimizer.step()
+
+        # Gather data and report
+        running_loss += loss.item()
+        if i % 1000 == 999:
+            last_loss = running_loss / 1000 # loss per batch
+            print('  batch {} loss: {}'.format(i + 1, last_loss))
+            tb_x = epoch_index * len(training_loader) + i + 1
+            tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+            running_loss = 0.
+
+    return last_loss
+
+# Initializing in a separate cell so we can easily add more epochs to the same run
+timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+writer = SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
+epoch_number = 0
+
+EPOCHS = 5
+
+best_vloss = 1_000_000.
+
+total_samples = len(validation_set)
+correct_samples = 0
+
+for epoch in range(EPOCHS):
+    print('EPOCH {}:'.format(epoch_number + 1))
+
+    # Make sure gradient tracking is on, and do a pass over the data
+    model.train(True)
+    avg_loss = train_one_epoch(epoch_number, writer)
+
+
+    running_vloss = 0.0
+    # Set the model to evaluation mode, disabling dropout and using population
+    # statistics for batch normalization.
+    model.eval()
+
+    # Disable gradient computation and reduce memory consumption.
+    with torch.no_grad():
+        for i, vdata in enumerate(validation_loader):
+            vinputs, vlabels = vdata
+            voutputs = model(vinputs)
+            vloss = loss_fn(voutputs, vlabels)
+            running_vloss += vloss
+            if(epoch == EPOCHS -1):
+                  _, pred = torch.max(voutputs, dim=1)
+                  correct_samples += pred.eq(vlabels).sum()
+
+    avg_vloss = running_vloss / (i + 1)
+    print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+
+    # Log the running loss averaged per batch
+    # for both training and validation
+    writer.add_scalars('Training vs. Validation Loss',
+                    { 'Training' : avg_loss, 'Validation' : avg_vloss },
+                    epoch_number + 1)
+    writer.flush()
+
+    # Track best performance, and save the model's state
+    if avg_vloss < best_vloss:
+        best_vloss = avg_vloss
+        model_path = 'model_{}_{}'.format(timestamp, epoch_number)
+        torch.save(model.state_dict(), model_path)
+
+    epoch_number += 1
+print(f'Val Accuracy: {correct_samples/total_samples}')
